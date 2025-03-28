8f6de67aefbb136598b14ef0331d10c5e2c68f54
NikitaVoliqLP <volanskijnikita2@gmail.com>
2025-03-28 20:04:13 +0100

    Added a new migration to fill the database

--- EntityFrameworkLesson.ConsoleApp/Program.cs
+++ EntityFrameworkLesson.ConsoleApp/Program.cs
@@ -8,30 +8,16 @@ namespace EntityFrameworkLesson.ConsoleApp
 
         private static FootballTeamRepository _repository;
 
-        /*static void Exercice3()
+        static void Main(string[] args)
         {
-            List<FootballTeam> teams = new List<FootballTeam> {
-                new FootballTeam { Name = "Manchester United", City = "Manchester", Wins = 3, Losses = 0, Draws = 2 },
-                new FootballTeam { Name = "Chelsea", City = "London", Wins = 1, Losses = 1, Draws = 3  },
-                new FootballTeam { Name = "Liverpool", City = "Liverpool", Wins = 0, Losses = 2, Draws = 0  },
-                new FootballTeam { Name = "Arsenal", City = "London", Wins = 2, Losses = 1, Draws = 2  }
-            };
-            _repository.AddRange(teams);
-
+            _repository = new FootballTeamRepository();
             List<FootballTeam> response = _repository.GetAll();
 
             Console.WriteLine("Teams:");
             foreach (FootballTeam team in response)
             {
-                Console.WriteLine($"Name: {team.Name}, City: {team.City}, Wins: {team.Wins}, Losses: {team.Losses}, Draws: {team.Draws}");
+                Console.WriteLine($"Name: {team.Name}, City: {team.City}, Wins: {team.Wins}, Losses: {team.Losses}, Draws: {team.Draws}, GoalsNumber: {team.GoalsNumber}, GoalsConcededNumber: {team.GoalsConcededNumber}");
             }
-            Console.ReadKey();
-        }*/
-
-        static void Main(string[] args)
-        {
-            _repository = new FootballTeamRepository();
-            //Exercice3();
         }
     }
 }
---
+++ EntityFrameworkLesson.DAL/Migrations/20250328185622_TableFill.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkLesson.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class TableFill : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            INSERT INTO FootballTeams (Name, City, Wins, Losses, Draws, GoalsNumber, GoalsConcededNumber)    
+            VALUES ('Manchester United', 'Manchester', 3, 0, 2, 10, 5),
+                   ('Chelsea', 'London', 1, 1, 3, 5, 5),
+                   ('Liverpool', 'Liverpool', 0, 2, 0, 2, 5),
+                   ('Arsenal', 'London', 4, 1, 2, 12, 2);
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
